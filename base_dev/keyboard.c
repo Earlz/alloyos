a73c74089427d3cf3a6ec946e2f266121b9b803e
hackr9483 <hackr9483@a0e77647-dd51-0410-b45c-2f38e4cc827b>
2008-08-25 03:02:42 +0000

    Added copyright header and file descriptions to most all files.

--- base_dev/keyboard.c
+++ base_dev/keyboard.c
@@ -1,3 +1,38 @@
+/*
+<Copyright Header>
+Copyright (c) 2007 - 2008 Jordan "Earlz/hckr83" Earls  <http://www.Earlz.biz.tm>
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+
+1. Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
+3. The name of the author may not be used to endorse or promote products
+   derived from this software without specific prior written permission.
+   
+THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL
+THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+This file is part of the AlloyOS project
+</Copyright Header>
+*/
+
+/**This has all of the platform and OS dependent keyboard functions -- Ring 0**/
+
+
 #include <x86.h>
 #include <stdlib.h>
 #include <kdebug.h>
@@ -216,10 +251,10 @@ void kbd_Irq1Handler(struct InterruptInfo *r){
 		if(kbd_DoShifts(tmp)==0){ //if not a shift-type key
 			
 			if ((kbd_shifts.caps^kbd_shifts.shift)==1) {
-				kd_putc_xy(kbdus_caps[tmp],11,12);
+				//kd_putc_xy(kbdus_caps[tmp],11,12);
 				kbd_PutBuffer(tmp,kbdus_caps[tmp]);
 			}else{
-				kd_putc_xy(kbdus[tmp],10,12);
+				//kd_putc_xy(kbdus[tmp],10,12);
 				kbd_PutBuffer(tmp,kbdus[tmp]);
 			}
 		}
